fe203621b5ba83efb01e7292447e570f668de444
Kyle Bernstein <kyle.bernstein@centro.net>
2022-01-10 16:48:47 -0500

    Documented cpu flags

--- src/constants.rs
+++ src/constants.rs
@@ -11,3 +11,20 @@ pub const DISPLAY_FACTOR: u32 = 2;
 
 // We can address from 0 - 65535 in memory (i.e. 0x0000 - 0xFFFF)
 pub const MEMORY_SIZE: Word = 65536;
+
+// Flags
+// The following bits are used with register A as a flags Register. The following bits
+// represent the following flags:
+// 7    S   Sign flag
+// 6	Z	Zero flag
+// 5    0   Unused
+// 4    A   Auxiliary Carry Flag (carry from bit 3)
+// 3    0   Unused
+// 2    P   Parity Flag
+// 1    1   Unused
+// 0    C   Carry Flag
+pub const SIGN_FLAG: usize = 7;
+pub const ZERO_FLAG: usize = 6;
+pub const AUXILIARY_CARRY_FLAG: usize = 4;
+pub const PARITY_FLAG: usize = 2;
+pub const CARRY_FLAG: usize = 0;

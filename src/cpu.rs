d5b5e2ec2b6e7690e4f88d150c5ccf050a422512
Kyle Bernstein <kyle.bernstein@centro.net>
2022-01-11 16:50:26 -0500

    Implemented a few more ops including DAA

--- src/cpu.rs
+++ src/cpu.rs
@@ -62,7 +62,11 @@ impl Cpu {
         self.program_counter = self.program_counter.wrapping_add(1);
 
         match opcode.operation {
+            Operation::CMA => self.do_complement_accumulator(&opcode),
             Operation::CMC => self.do_complement_carry(&opcode),
+            Operation::DAA => self.do_decimal_adjust_accumulator(&opcode),
+            Operation::DCR => self.do_decrement(&opcode),
+            Operation::INR => self.do_increment(&opcode),
             Operation::NOP => opcode.cycles,
             Operation::STC => self.do_set_carry(&opcode),
             _ => panic!("Unexpected opcode 0x{:02X} encountered", op),
@@ -188,9 +192,72 @@ impl Cpu {
         }
     }
 
+    fn do_complement_accumulator(&mut self, opcode: &OpCode) -> u8 {
+        unsafe {
+            self.af.parts.hi = !self.af.parts.hi;
+            opcode.cycles
+        }
+    }
+
     fn do_complement_carry(&mut self, opcode: &OpCode) -> u8 {
         self.update_carry_flag(!self.is_carry_flag_set());
         opcode.cycles
+    }
+
+    fn do_decimal_adjust_accumulator(&mut self, opcode: &OpCode) -> u8 {
+        // There is a description of how this algorithm works in the official
+        // 8080 documentation/manual
+
+        unsafe {
+            let mut val = self.af.parts.hi;
+
+            if self.is_auxiliary_carry_flag_set() || (val & 0xF) > 0x09 {
+                self.update_auxiliary_carry_flag((val & 0xF) + 6 > 0xF);
+                val = val.wrapping_add(6);
+            }
+
+            if self.is_carry_flag_set() || (val >> 4) > 0x09 {
+                self.update_carry_flag(((val >> 4) + 6) > 0xF);
+                let upper = ((val >> 4) + 6) & 0xF;
+                let lower = val & 0xF;
+                val = (upper << 4) | lower;
+            }
+
+            self.update_parity_flag(is_even_parity(&val));
+            self.update_sign_flag(is_bit_set(&val, 7));
+            self.update_zero_flag(val == 0);
+
+            self.af.parts.hi = val;
+
+            opcode.cycles
+        }
+    }
+
+    fn do_decrement(&mut self, opcode: &OpCode) -> u8 {
+        unsafe {
+            let res = match opcode.code {
+                0x05 => { self.bc.parts.hi = self.bc.parts.hi.wrapping_sub(1); self.bc.parts.hi },
+                0x0D => { self.bc.parts.lo = self.bc.parts.lo.wrapping_sub(1); self.bc.parts.lo },
+                0x15 => { self.de.parts.hi = self.de.parts.hi.wrapping_sub(1); self.de.parts.hi },
+                0x1D => { self.de.parts.lo = self.de.parts.lo.wrapping_sub(1); self.de.parts.lo },
+                0x25 => { self.hl.parts.hi = self.hl.parts.hi.wrapping_sub(1); self.hl.parts.hi },
+                0x2D => { self.hl.parts.lo = self.hl.parts.lo.wrapping_sub(1); self.hl.parts.lo },
+                0x35 => {
+                    let val = self.read_memory(self.hl.val).wrapping_sub(1);
+                    self.write_memory(self.hl.val, val);
+                    val
+                },
+                0x3D => { self.af.parts.hi = self.bc.parts.hi.wrapping_sub(1); self.af.parts.hi },
+                _ => panic!("Unexpected code [{:02X}] encountered for DCR", opcode.code)
+            };
+
+            self.update_zero_flag(res == 0);
+            self.update_sign_flag(is_bit_set(&res, 7));
+            self.update_auxiliary_carry_flag(res & 0xF == 0xF);
+            self.update_parity_flag(is_even_parity(&res));
+
+            opcode.cycles
+        }
     }
 
     fn do_increment(&mut self, opcode: &OpCode) -> u8 {
@@ -229,6 +296,85 @@ impl Cpu {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_do_complement_accumulator() {
+        let mut cpu = Cpu::new(Bus::new());
+        let code = 0x2F;
+        let opcode = OPCODE_MAP.get(&code).unwrap();
+
+        cpu.af.parts.hi = 0b10101010;
+        cpu.do_complement_accumulator(&opcode);
+        unsafe {
+            assert_eq!(cpu.af.parts.hi, 0b01010101);
+        }
+    }
+
+    #[test]
+    fn test_do_decimal_adjust_accumulator() {
+        let mut cpu = Cpu::new(Bus::new());
+        let code = 0x27;
+        let opcode = OPCODE_MAP.get(&code).unwrap();
+
+        cpu.af.parts.hi = 0x9B;
+        cpu.do_decimal_adjust_accumulator(&opcode);
+
+        unsafe {
+            assert_eq!(cpu.af.parts.hi, 0x01);
+            assert_eq!(cpu.is_zero_flag_set(), false);
+            assert_eq!(cpu.is_sign_flag_set(), false);
+            assert_eq!(cpu.is_carry_flag_set(), true);
+            assert_eq!(cpu.is_auxiliary_carry_flag_set(), true);
+            assert_eq!(cpu.is_parity_flag_set(), false);
+        }
+    }
+
+    #[test]
+    fn test_do_decrement() {
+        let mut cpu = Cpu::new(Bus::new());
+        let code = 0x05;
+        let opcode = OPCODE_MAP.get(&code).unwrap();
+
+        cpu.bc.parts.hi = 0;
+        cpu.do_decrement(&opcode);
+        unsafe {
+            assert_eq!(cpu.bc.parts.hi, 0xFF);
+            assert_eq!(cpu.is_zero_flag_set(), false);
+            assert_eq!(cpu.is_sign_flag_set(), true);
+            assert_eq!(cpu.is_auxiliary_carry_flag_set(), true);
+            assert_eq!(cpu.is_parity_flag_set(), true);
+        }
+
+        cpu.bc.parts.hi = 1;
+        cpu.do_decrement(&opcode);
+        unsafe {
+            assert_eq!(cpu.bc.parts.hi, 0);
+            assert_eq!(cpu.is_zero_flag_set(), true);
+            assert_eq!(cpu.is_sign_flag_set(), false);
+            assert_eq!(cpu.is_auxiliary_carry_flag_set(), false);
+            assert_eq!(cpu.is_parity_flag_set(), true);
+        }
+
+        cpu.bc.parts.hi = 0x80;
+        cpu.do_decrement(&opcode);
+        unsafe {
+            assert_eq!(cpu.bc.parts.hi, 0x7F);
+            assert_eq!(cpu.is_zero_flag_set(), false);
+            assert_eq!(cpu.is_sign_flag_set(), false);
+            assert_eq!(cpu.is_auxiliary_carry_flag_set(), true);
+            assert_eq!(cpu.is_parity_flag_set(), false);
+        }
+
+        cpu.bc.parts.hi = 0x7F;
+        cpu.do_decrement(&opcode);
+        unsafe {
+            assert_eq!(cpu.bc.parts.hi, 0x7E);
+            assert_eq!(cpu.is_zero_flag_set(), false);
+            assert_eq!(cpu.is_sign_flag_set(), false);
+            assert_eq!(cpu.is_auxiliary_carry_flag_set(), false);
+            assert_eq!(cpu.is_parity_flag_set(), true);
+        }
+    }
 
     #[test]
     fn test_do_increment() {
--- src/ops.rs
+++ src/ops.rs
@@ -8,7 +8,10 @@ use crate::utils::*;
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum Operation {
+    CMA,
     CMC,
+    DAA,
+    DCR,
     INR,
     NOP,
     STC,
@@ -47,17 +50,27 @@ lazy_static! {
     pub static ref CPU_OP_CODES: Vec<OpCode> = vec![
         OpCode::new(0x00, String::from("NOP"), Operation::NOP, 1, 4, None),
         OpCode::new(0x04, String::from("INR B"), Operation::INR, 1, 5, None),
+        OpCode::new(0x05, String::from("DCR B"), Operation::DCR, 1, 5, None),
         OpCode::new(0x0C, String::from("INR C"), Operation::INR, 1, 5, None),
+        OpCode::new(0x0D, String::from("DCR C"), Operation::DCR, 1, 5, None),
 
         OpCode::new(0x14, String::from("INR D"), Operation::INR, 1, 5, None),
+        OpCode::new(0x15, String::from("DCR D"), Operation::DCR, 1, 5, None),
         OpCode::new(0x1C, String::from("INR E"), Operation::INR, 1, 5, None),
+        OpCode::new(0x1D, String::from("DCR E"), Operation::DCR, 1, 5, None),
 
         OpCode::new(0x24, String::from("INR H"), Operation::INR, 1, 5, None),
+        OpCode::new(0x25, String::from("DCR H"), Operation::DCR, 1, 5, None),
+        OpCode::new(0x27, String::from("DAA"), Operation::DAA, 1, 4, None),
         OpCode::new(0x2C, String::from("INR L"), Operation::INR, 1, 5, None),
+        OpCode::new(0x2D, String::from("DCR L"), Operation::DCR, 1, 5, None),
+        OpCode::new(0x2F, String::from("CMA"), Operation::CMA, 1, 4, None),
 
         OpCode::new(0x34, String::from("INR M"), Operation::INR, 1, 10, None),
+        OpCode::new(0x35, String::from("DCR M"), Operation::DCR, 1, 10, None),
         OpCode::new(0x37, String::from("STC"), Operation::STC, 1, 4, None),
         OpCode::new(0x3C, String::from("INR A"), Operation::INR, 1, 5, None),
+        OpCode::new(0x3D, String::from("DCR A"), Operation::DCR, 1, 5, None),
         OpCode::new(0x3F, String::from("CMC"), Operation::CMC, 1, 4, None),
     ];
 
